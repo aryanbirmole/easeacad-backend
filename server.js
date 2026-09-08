7f0ded9f19614c3bd1db7693c23246c1cf29d688
aryanbirmole <aryanbirmole@gmail.com>
2026-09-08 20:29:41 +0530

    Render Deployment Changes

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ app.get('/hello', (req, res) => {
   res.status(200).json({ success: true, message: 'Backend is working!' });
 });
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on http://localhost:${process.env.PORT}`);
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
